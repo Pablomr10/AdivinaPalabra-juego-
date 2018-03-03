6d0058ed0490c6c6898dfac3c7bb53f6e0f04c0f
Pablomr10 <33936611+Pablomr10@users.noreply.github.com>
2018-03-03 20:48:08 +0100

    Primer contacto con el codigo.
    He añadido a esta rama el codigo para modificarlo e ir avanzando.

---
+++ AdivinaPalabra.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class AdivinaPalabra{
+  public static void main (String []args){
+    Scanner teclado=new Scanner(System.in);
+    String  pal = "hola";
+    String Palabra;
+    while (){
+      System.out.println ("Ponga la palabra que desea comparar ");
+    Palabra = teclado.next();
+    if (Palabra.equals(pal))
+      System.out.println ("Correcto");
+      else
+        System.out.println ("Prueba de nuevo");
+      }
+  }
+}
